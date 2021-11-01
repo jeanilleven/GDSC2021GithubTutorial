d47b64e4582c2d1dded13a524dccf3eb7243960f
Laplace <johnalexisbuot@gmail.com>
2021-11-01 20:18:56 +0800

    [Fix][Lex] Adjusted Active State Translate of Button

--- src/components/common/Buttons/Button.jsx
+++ src/components/common/Buttons/Button.jsx
@@ -2,7 +2,7 @@ function Button({ className, children, RightIcon, LeftIcon, ...props }) {
   const textStyles = "bg-primary-200 font-bold text-white";
   const containerStyles = "rounded-lg shadow-xl py-4 px-8";
   const flexStyles = "flex justify-center items-center";
-  const stateStyles = "hover:opacity-80 disabled:opacity-50 active:translate-y-1 transform";
+  const stateStyles = "hover:opacity-80 disabled:opacity-50 active:translate-y-0.5 transform";
   const transitionStyle = "transition-all duration-250 ease-in-out";
 
   const styles = `${textStyles} ${stateStyles} ${containerStyles} ${transitionStyle} ${flexStyles}`;
