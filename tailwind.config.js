c992d1b7f3e3efbee1aa7deaf7627dede61c9f9b
Laplace <johnalexisbuot@gmail.com>
2021-11-01 15:47:58 +0800

    [Feature][Lex] Added Colors Styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+			colors: {
+				primary: {
+					200: "hsl(26, 100%, 55%)",
+					100: "hsl(25, 100%, 94%)",
+				},
+				neutral: {
+					400: "hsl(220, 13%, 13%)",
+					300: "hsl(219, 9%, 45%)",
+					200: "hsl(220, 14%, 75%)",
+					100: "hsl(223, 64%, 98%)",
+				},
+				white: "hsl(0, 0%, 100%)",
+				dark: "hsl(0, 0%, 0%)",
+
+			}
+		},
   },
   variants: {
     extend: {},
