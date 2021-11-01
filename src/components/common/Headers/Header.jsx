e14c85a8dc293bd8cfe5aa550c0ff51928e08c90
Laplace <johnalexisbuot@gmail.com>
2021-11-01 20:45:20 +0800

    [Feature][Lex] Changed letter spacing of Header for larger screens

--- src/components/common/Headers/Header.jsx
+++ src/components/common/Headers/Header.jsx
@@ -1,6 +1,9 @@
 function Header({ children, className, ...props }) {
   return (
-    <h1 {...props} className={"font-bold text-3xl text-neutral-400 leading-8 lg:text-5xl " + className}>
+    <h1
+      {...props}
+      className={"font-bold text-3xl text-neutral-400 leading-8 lg:text-5xl lg:tracking-tight " + className}
+    >
       {children}
     </h1>
   );
