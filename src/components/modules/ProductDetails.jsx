bc31779b5d7e002c22869377c4600be306046910
Laplace <johnalexisbuot@gmail.com>
2021-11-01 22:03:24 +0800

    [Fix][Lex] Renamed props of ProductDetails for Readability

--- src/components/modules/ProductDetails.jsx
+++ src/components/modules/ProductDetails.jsx
@@ -1,11 +1,11 @@
 import Text from "../common/Text";
 import { Header, SubHeader } from "./../common/Headers";
 
-function ProductDetails({ title, subtitle, description, ...props }) {
+function ProductDetails({ name, company, description, ...props }) {
   return (
     <article {...props}>
-      {subtitle && <SubHeader className="mb-3 lg:mb-4">{subtitle}</SubHeader>}
-      {title && <Header className="mb-4 lg:mb-9">{title}</Header>}
+      {company && <SubHeader className="mb-3 lg:mb-4">{company}</SubHeader>}
+      {name && <Header className="mb-4 lg:mb-9">{name}</Header>}
       {description && <Text>{description}</Text>}
     </article>
   );
