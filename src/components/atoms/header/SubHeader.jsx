e58f13ef40e93de2da36ac72ce3c8cf22dfb5244
Laplace <johnalexisbuot@gmail.com>
2021-11-01 17:54:49 +0800

    [Feature][Lex] SubHeader Component Created

---
+++ src/components/atoms/header/SubHeader.jsx
@@ -0,0 +1,9 @@
+function SubHeader({ props, children }) {
+  return (
+    <h2 {...props} className="font-bold tracking-widest text-xs uppercase text-primary-200 mb-3">
+      {children}
+    </h2>
+  );
+}
+
+export default SubHeader;
