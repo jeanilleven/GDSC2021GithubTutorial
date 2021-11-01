e9bb5ceaa0c00a2bdd3d1ed77f6b73bc8eaacbf6
Laplace <johnalexisbuot@gmail.com>
2021-11-01 20:43:20 +0800

    [Feature][Lex] Changed size of Header for larger screens

--- src/components/common/Headers/Header.jsx
+++ src/components/common/Headers/Header.jsx
@@ -1,6 +1,6 @@
 function Header({ children, className, ...props }) {
   return (
-    <h1 {...props} className={"font-bold text-3xl text-neutral-400 leading-8 " + className}>
+    <h1 {...props} className={"font-bold text-3xl text-neutral-400 leading-8 lg:text-5xl " + className}>
       {children}
     </h1>
   );
