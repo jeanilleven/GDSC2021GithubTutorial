bfbeb0a62e475b32a02bc710d1a319691b738af8
Laplace <johnalexisbuot@gmail.com>
2021-11-01 20:16:53 +0800

    [Feat][Lex] Added State Styles to Icon Button

--- src/components/common/Buttons/IconButton.jsx
+++ src/components/common/Buttons/IconButton.jsx
@@ -1,6 +1,12 @@
 function IconButton({ Icon, className, ...props }) {
+  const textStyles = "text-primary-200 text-md font-bold";
+  const stateStyles = "hover:opacity-70 disabled:opacity-50 active:translate-y-0.5 transform";
+  const containerStyles = "rounded-lg py-4 px-6";
+  const transitionStyles = "transition-all duration-250 ease-in-out";
+
+  const styles = `${stateStyles} ${textStyles} ${containerStyles} ${transitionStyles}`;
   return (
-    <button {...props} className={"text-primary-200 text-md font-bold rounded-lg py-4 px-6 " + className}>
+    <button {...props} className={styles + " " + className}>
       {Icon}
     </button>
   );
