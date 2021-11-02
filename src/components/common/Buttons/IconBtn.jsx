518d039b8c49354a422332f42d2bdc87e2d902f2
Laplace <johnalexisbuot@gmail.com>
2021-11-02 17:53:21 +0800

    [Fix][Lex] Rewrote IconBtn component to be more generalized

--- src/components/common/Buttons/IconBtn.jsx
+++ src/components/common/Buttons/IconBtn.jsx
@@ -1,12 +1,11 @@
 import Btn from "./Btn";
 
-function IconBtn({ Icon, className, ...props }) {
-  const textStyles = "text-primary-200 text-md font-bold";
-  const activeStyles = "active:translate-y-0.5 transform";
-  const containerStyles = "rounded-lg py-4 px-6";
+function IconBtn({ Icon, className, iconColor = "primary-200", bgColor = "transparent", ...props }) {
+  const textStyles = `text-${iconColor} text-md font-bold`;
+  const containerStyles = `bg-${bgColor} py-4 px-6`;
   const transitionStyles = "transition-all duration-250 ease-in-out";
 
-  const styles = `${activeStyles} ${textStyles} ${containerStyles} ${transitionStyles}`;
+  const styles = `${textStyles} ${containerStyles} ${transitionStyles}`;
   return (
     <Btn {...props} className={styles + " " + className}>
       {Icon}
--- src/components/modules/Counter.jsx
+++ src/components/modules/Counter.jsx
@@ -5,9 +5,19 @@ import { FlexCenter } from "../common/Layouts";
 function Counter({ className, count, onIncrement, onDecrement, ...props }) {
   return (
     <FlexCenter {...props} className={"py-1 justify-between bg-neutral-100 rounded-md " + className}>
-      <IconBtn name="Decrement" onClick={onDecrement} Icon={<FaMinus />} />
+      <IconBtn
+        className="rounded-lg active:translate-y-0.5 transform"
+        name="Decrement"
+        onClick={onDecrement}
+        Icon={<FaMinus />}
+      />
       <span className="text-neutral-400 font-bold">{count}</span>
-      <IconBtn name="Increment" onClick={onIncrement} Icon={<FaPlus />} />
+      <IconBtn
+        className="rounded-lg active:translate-y-0.5 transform"
+        name="Increment"
+        onClick={onIncrement}
+        Icon={<FaPlus />}
+      />
     </FlexCenter>
   );
 }
