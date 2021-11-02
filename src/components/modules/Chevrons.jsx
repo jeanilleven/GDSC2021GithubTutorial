15a7b10300ec3ea0219b286d8caf980023ba08d2
Laplace <johnalexisbuot@gmail.com>
2021-11-02 21:03:59 +0800

    [Fix][Lex] Added Styling prop for right and left chevron

--- src/components/modules/Chevrons.jsx
+++ src/components/modules/Chevrons.jsx
@@ -1,7 +1,7 @@
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import { IconBtn } from "../common/Buttons";
 
-function Chevrons({ onNext, onPrev, className, size = 16 }) {
+function Chevrons({ onNext, onPrev, className, nextStyle, prevStyle, size = 16 }) {
   const stateStyles = "hover:opacity-100 hover:text-primary-200";
   const positionStyles = "absolute top-1/2 -translate-y-1/2";
   const baseStyles = "bg-neutral-100 text-neutral-400 rounded-3xl";
@@ -13,13 +13,13 @@ function Chevrons({ onNext, onPrev, className, size = 16 }) {
         name="Next"
         Icon={<FaChevronRight size={size} />}
         onClick={onNext}
-        className={styles + " right-0"}
+        className={styles + " right-0 " + nextStyle}
       />
       <IconBtn
         name="Previous"
         Icon={<FaChevronLeft size={size} />}
         onClick={onPrev}
-        className={styles + " left-0"}
+        className={styles + " left-0 " + prevStyle}
       />
     </>
   );
