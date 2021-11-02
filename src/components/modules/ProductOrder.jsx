2668b259bf62dfdff26573c5ab2670a0f2109bbc
Laplace <johnalexisbuot@gmail.com>
2021-11-02 22:11:18 +0800

    [Feature][Lex] Created ProductOrder Component

---
+++ src/components/modules/ProductOrder.jsx
@@ -0,0 +1,26 @@
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { PrimaryBtn } from "./../common/Buttons";
+import { FlexCenter } from "./../common/Layouts";
+import Counter from "./Counter";
+
+function ProductOrder({ quantity, onIncrement, onDecrement, ...props }) {
+  return (
+    <FlexCenter {...props}>
+      <Counter
+        count={quantity}
+        onIncrement={onIncrement}
+        onDecrement={onDecrement}
+        className="w-full lg:mr-4 lg:w-2/3"
+      />
+      <PrimaryBtn
+        name="Add To Cart"
+        className="mt-4 w-full lg:mt-0"
+        LeftIcon={<AiOutlineShoppingCart className="mr-2" size={20} />}
+      >
+        Add to Cart
+      </PrimaryBtn>
+    </FlexCenter>
+  );
+}
+
+export default ProductOrder;
