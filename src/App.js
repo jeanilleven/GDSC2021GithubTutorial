eb69b5d8dbe8dd59cecb9410eb358abf9c496d21
Laplace <johnalexisbuot@gmail.com>
2021-11-02 22:29:47 +0800

    [Feature][Lex] Added Product Details for Shoe Product

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import ProductPage from "./components/pages/ProductPage";
+import { getImages } from "./services/imageService";
+
+const images = getImages();
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <ProductPage
+      name="Fall Limited Edition Sneakers"
+      company="Sneaker Company"
+      description="These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer."
+      price={250}
+      discount={0.5}
+      quantity={0}
+      images={images}
+      // onOrder
+      // onIncrement
+      // onDecrement
+    />
   );
 }
 
