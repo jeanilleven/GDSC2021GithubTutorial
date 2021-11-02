87a94b4d6796ce5c501af3a1172e8bedb6ab2893
Laplace <johnalexisbuot@gmail.com>
2021-11-02 20:30:38 +0800

    [Fix][Lex] Fixed wonky transform animations in Lightbox

--- src/components/common/LightBox.jsx
+++ src/components/common/LightBox.jsx
@@ -1,6 +1,6 @@
 function LightBox({ children, className, isOpen, ...props }) {
   const baseStyles = "fixed w-full bg-black bg-opacity-75";
-  const transitionStyles = "transition-all duration-500 ease-in-out";
+  const transitionStyles = "transition-opacity duration-500 ease-in-out";
 
   const openStyles = "overflow-auto top-0 bottom-0 opacity-100";
   const hiddenStyles = "overflow-hidden -top-full opacity-0";
