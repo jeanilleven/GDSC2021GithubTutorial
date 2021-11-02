157b90dc3b937131542e0c623aae4ce59e396cc4
Laplace <johnalexisbuot@gmail.com>
2021-11-02 20:46:25 +0800

    [Fix][Lex] Directory typo

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,4 +1,4 @@
 export { default as Badge, default as LightBox } from "./Badge";
 export { default as Image } from "./Image";
-export { default as PriceTag } from "./Pricetag";
+export { default as PriceTag } from "./PriceTag";
 export { default as Text } from "./Text";
