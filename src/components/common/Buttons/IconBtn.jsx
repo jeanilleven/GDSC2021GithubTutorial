a5c858bf87428b9d2a086e9191f37af193af74cd
Laplace <johnalexisbuot@gmail.com>
2021-11-02 18:05:01 +0800

    [Fix][Lex] Removed default padding in IconBtn

--- src/components/common/Buttons/IconBtn.jsx
+++ src/components/common/Buttons/IconBtn.jsx
@@ -1,11 +1,10 @@
 import Btn from "./Btn";
 
 function IconBtn({ Icon, className, iconColor = "primary-200", bgColor = "transparent", ...props }) {
-  const textStyles = `text-${iconColor} text-md font-bold`;
-  const containerStyles = `bg-${bgColor} py-4 px-6`;
+  const baseStyles = `bg-${bgColor} text-${iconColor} text-md font-bold`;
   const transitionStyles = "transition-all duration-250 ease-in-out";
 
-  const styles = `${textStyles} ${containerStyles} ${transitionStyles}`;
+  const styles = `${baseStyles} ${transitionStyles}`;
   return (
     <Btn {...props} className={styles + " " + className}>
       {Icon}
