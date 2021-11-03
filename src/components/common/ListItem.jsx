2d208a07c98d95b311557e588e9ae6fa725a6401
Laplace <johnalexisbuot@gmail.com>
2021-11-03 08:32:16 +0800

    [Feature][Lex] Created ListItem component

---
+++ src/components/common/ListItem.jsx
@@ -0,0 +1,13 @@
+function ListItem({ items, keyExtractor, ListComponent, className, ...props }) {
+  const styles = ["list-none", className];
+
+  return (
+    <ul {...props} className={styles.flat(Infinity).join(" ")}>
+      {items.map((item) => (
+        <li key={keyExtractor(item)}>{ListComponent(item)}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default ListItem;
--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,5 +1,6 @@
 export { default as Badge } from "./Badge";
 export { default as Image } from "./Image";
 export { default as LightBox } from "./LightBox";
+export { default as ListItem } from "./ListItem";
 export { default as PriceTag } from "./PriceTag";
 export { default as Text } from "./Text";
