b51cbb45f6110fd581fc094fadb0bf98de593a91
Laplace <johnalexisbuot@gmail.com>
2021-11-03 09:34:24 +0800

    [Fix][Lex] Extracted the Lightbox component from Product Section to fit definition of a section

--- src/components/modules/Product/ProductSection.jsx
+++ src/components/modules/Product/ProductSection.jsx
@@ -1,56 +1,53 @@
-import { useState } from "react";
-import { ProductCarousel, ProductDetails, ProductLightbox, ProductOrder } from ".";
+import { ProductCarousel, ProductDetails, ProductOrder } from ".";
 import { ImageBtn } from "../../common/Buttons";
 
-function ProductSection({ product, quantity, onOrder, onIncrement, onDecrement }) {
+function ProductSection({
+  product,
+  quantity,
+  image,
+  onImageSelect,
+  onOrder,
+  onIncrement,
+  onDecrement,
+  onImageClick,
+}) {
   const { name, company, description, price, discount, images } = product;
-  const [currentImg, setCurrentImg] = useState(images[0]);
-  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
 
   return (
-    <main>
-      <section className="flex flex-col lg:flex-row max-w-screen-lg m-auto lg:pt-32">
-        <ProductCarousel
-          images={images}
-          selected={currentImg}
-          onSelect={setCurrentImg}
-          className="lg:mr-28"
-          chevronStyles="p-3 mx-4 lg:hidden active:scale-125 transform"
-          Display={(img) => (
-            <ImageBtn
-              src={img.imageUrl}
-              alt={img.name}
-              onClick={() => setIsLightboxOpen((toggle) => !toggle)}
-              className="rounded-none cursor-default hover:opacity-100 lg:cursor-pointer lg:rounded-2xl"
-            />
-          )}
-        />
-        <section className="font-display py-4 px-2 m-auto w-11/12 lg:p-0 lg:mt-14 lg:ml-4 lg:w-full">
-          <ProductDetails
-            name={name}
-            company={company}
-            description={description}
-            price={price}
-            discount={discount}
-          />
-          <ProductOrder
-            quantity={quantity}
-            onOrder={onOrder}
-            onIncrement={onIncrement}
-            onDecrement={onDecrement}
-            className="lg:flex-row justify-center mt-5 lg:mt-9"
-            col
-          />
-        </section>
-      </section>
-      <ProductLightbox
-        isOpen={isLightboxOpen}
-        onClose={() => setIsLightboxOpen((toggle) => !toggle)}
+    <section className="flex flex-col lg:flex-row max-w-screen-lg m-auto lg:pt-32">
+      <ProductCarousel
         images={images}
-        selected={currentImg}
-        onSelect={setCurrentImg}
+        selected={image}
+        onSelect={onImageSelect}
+        className="lg:mr-28"
+        chevronStyles="p-3 mx-4 lg:hidden active:scale-125 transform"
+        Display={(img) => (
+          <ImageBtn
+            src={img.imageUrl}
+            alt={img.name}
+            onClick={onImageClick}
+            className="rounded-none cursor-default hover:opacity-100 lg:cursor-pointer lg:rounded-2xl"
+          />
+        )}
       />
-    </main>
+      <section className="font-display py-4 px-2 m-auto w-11/12 lg:p-0 lg:mt-14 lg:ml-4 lg:w-full">
+        <ProductDetails
+          name={name}
+          company={company}
+          description={description}
+          price={price}
+          discount={discount}
+        />
+        <ProductOrder
+          quantity={quantity}
+          onOrder={onOrder}
+          onIncrement={onIncrement}
+          onDecrement={onDecrement}
+          className="lg:flex-row justify-center mt-5 lg:mt-9"
+          col
+        />
+      </section>
+    </section>
   );
 }
 
--- src/components/pages/ProductPage.jsx
+++ src/components/pages/ProductPage.jsx
@@ -1,16 +1,30 @@
-import { ProductSection } from "../modules/Product";
+import { useState } from "react";
+import { ProductLightbox, ProductSection } from "../modules/Product";
 
 function ProductPage({ product }) {
+  const [currentImg, setCurrentImg] = useState(product.images[0]);
+  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
+
   return (
-    <>
+    <main>
       <ProductSection
         product={product}
+        image={currentImg}
+        onImageSelect={setCurrentImg}
+        onImageClick={() => setIsLightboxOpen((v) => !v)}
         quantity={0}
         // onOrder
         // onIncrement
         // onDecrement
       />
-    </>
+      <ProductLightbox
+        isOpen={isLightboxOpen}
+        onClose={() => setIsLightboxOpen((v) => !v)}
+        images={product.images}
+        selected={currentImg}
+        onSelect={setCurrentImg}
+      />
+    </main>
   );
 }
 
