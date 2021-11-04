f5c976615790f6938736cbbdd36398a0f1ff3a70
Laplace <johnalexisbuot@gmail.com>
2021-11-05 02:17:18 +0800

    [Fix][Lex] Removed styling of PriceTag for generalization

--- src/components/modules/Price.jsx
+++ src/components/modules/Price.jsx
@@ -5,15 +5,18 @@ function Price({ value, discount, className, ...props }) {
   const discountedPrice = discount ? value * discount : 0;
   const disPercentage = discount ? (discount * 100).toString() + "%" : 0;
 
+  const crossedOutStyle = "text-neutral-200 font-bold line-through";
+  const defaultStyle = "text-neutral-400 text-3xl font-bold";
+
   return (
     <FlexCenter {...props} className={["justify-between", "lg:flex-col lg:items-start", className]}>
       {discount && (
         <FlexCenter className="lg:mb-1">
-          <PriceTag value={discountedPrice} className="mr-4" />
+          <PriceTag value={discountedPrice} className={`${defaultStyle} mr-4`} />
           <Badge text={disPercentage} />
         </FlexCenter>
       )}
-      <PriceTag value={value} crossedOut={discount} />
+      <PriceTag value={value} className={discount ? crossedOutStyle : defaultStyle} />
     </FlexCenter>
   );
 }
