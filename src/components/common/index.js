cc90a0c2f8bc49762f72959b23e6885755464ae0
Laplace <johnalexisbuot@gmail.com>
2021-11-04 21:32:24 +0800

    [Fix][Lex] Incorrect referencing of Lightbox

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,6 +1,6 @@
 export { default as Badge } from "./Badge";
 export { default as Image } from "./Image";
-export { default as Lightbox } from "./LightBox";
+export { default as Lightbox } from "./Lightbox";
 export { default as ListItem } from "./ListItem";
 export { default as PriceTag } from "./PriceTag";
 export { default as Text } from "./Text";
